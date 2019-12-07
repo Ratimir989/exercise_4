7e994c14a1bcb775042bc749b17bc35a7499171d
Ratimir <ra0953029364@gmail.com>
2019-12-07 18:59:56 +0200

    Добавьте файлы проекта.

---
+++ exercise_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 0 или 1");
+            int num = int.Parse(Console.ReadLine());
+            string a = num == 1 ? "Хорошо" : num == 0 ? "Плохо" : "Введёное число не является 1 или 0";
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+                      
+                
+            
+        
+    }
+}
